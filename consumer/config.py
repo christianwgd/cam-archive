d3be89161dbadc3fefe81b9f3d4f5d17125e6878
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-03-04 11:18:22 +0100

    Remove consumer config from tracking

--- consumer/config.py
+++
@@ -1,3 +0,0 @@
-python_executable = "/Users/christianwiegand/.virtualenvs/cam-archive/bin/python"
-directory_to_watch = "/Users/christianwiegand/test"
-target_directory = "/Users/christianwiegand/PycharmProjects/cam-archive/media/videos"
