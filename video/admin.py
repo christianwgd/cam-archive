f3425863f2a7e23aa72d61b65c8d657aa34475c1
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-03-04 21:59:54 +0100

    Add admin action to generate thumb

--- video/admin.py
+++ video/admin.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import gettext_lazy as _
 
-from video.models import Video
+from video.models import Video, create_thumbnail
 
 
 @admin.register(Video)
@@ -9,3 +10,11 @@ class VideoAdmin(admin.ModelAdmin):
     list_display = ['name', 'file', 'timestamp']
     date_hierarchy = 'timestamp'
     search_fields = ['name']
+    actions = ['generate_thumbnail']
+
+    @admin.action(description=_('Generate thumbnail for video'))
+    def generate_thumbnail(self, request, queryset):
+        for obj in queryset:
+            create_thumbnail(obj)
+            obj.save()
+            messages.success(request, _('Successfully generated thumbnail for "%s"' % obj))
