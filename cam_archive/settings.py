27d0c79bff926a7b40e3bb6f23501d3703d7ba20
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-03-04 11:18:01 +0100

    Fix typo in db namer

--- cam_archive/settings.py
+++ cam_archive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'  # noqa: S1
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': Path(BASE_DIR) / 'cam_arvhive.db',  # Or path to database file if using sqlite3.
+        'NAME': Path(BASE_DIR) / 'cam_archive.db',  # Or path to database file if using sqlite3.
     },
 }
 
