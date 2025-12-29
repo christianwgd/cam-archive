55ff421c8dbb3859bd20a517ec0cd046a93ad16d
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-12-29 16:16:01 +0100

    Add camera filter on video list

---
+++ video/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+
+from video.models import Video
+
+
+class VideoFilter(django_filters.FilterSet):
+    class Meta:
+        model = Video
+        fields = [
+            'camera'
+        ]
+
+    camera = django_filters.AllValuesFilter(field_name='camera__name')
--- video/views.py
+++ video/views.py
@@ -4,18 +4,21 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.decorators.http import require_GET
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
+from django_filters.views import FilterView
 
+from video.filters import VideoFilter
 from video.models import Video, Ring
 
 logger = getLogger('cam_archive')
 
 
-class VideoListView(LoginRequiredMixin, ListView):
+class VideoListView(LoginRequiredMixin, FilterView):
     model = Video
     template_name = 'video/video_list.html'
     context_object_name = 'video_list'
     ordering = ['-timestamp']
+    filterset_class = VideoFilter
 
 
 class VideoDetailView(LoginRequiredMixin, DetailView):
